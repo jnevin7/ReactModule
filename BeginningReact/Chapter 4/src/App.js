0ed7d9eca7f0951faf672210950a2ca81b622385
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2024-05-16 19:54:30 +0200

    Add files via upload

---
+++ BeginningReact/Chapter 4/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Products from './Products';
+import Rating from "./Rating";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Products />
+    </div>
+  );
+}
+
+export default App;
