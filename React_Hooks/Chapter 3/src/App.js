b74131e5225de426c6880dbba83638da52f7f9e5
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2024-05-27 18:49:46 +0200

    Add files via upload

---
+++ React_Hooks/Chapter 3/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Products from './Products';
+import { Button } from 'react-bootstrap';
+import Rating from './Rating';
+function App() {
+  return (
+    <div>
+      <Rating rating='1' />
+      <Rating rating='2' />
+      <Rating rating='3' />
+      <Rating rating='4' />
+      <Rating rating='5' />
+    </div>
+  );
+}
+export default App;
