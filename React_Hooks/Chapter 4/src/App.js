cc124747205f9e8a1ad20ae6aacc1860507fc3dd
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2024-05-27 19:05:25 +0200

    Add files via upload

---
+++ React_Hooks/Chapter 4/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Products from './Products';
+function App() {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+}
+export default App;
